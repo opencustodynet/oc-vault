9e4d25a492805fb28bfe178ef52f441d12953215
hosseinpro <hossein.rezaei.ghaleh@gmail.com>
2024-05-17 18:18:24 +0000

    move mocks to core

--- vault-api/src/main.rs
+++ vault-api/src/main.rs
@@ -1,24 +1,50 @@
 #[macro_use]
 extern crate rocket;
-
+use core::slice;
 use rocket::serde::json::Json;
 use serde_json::{json, Value};
 
-mod core_mock;
+pub const FM_MAX_BUFFER_SIZE: usize = 1024 * 64;
 
 #[post("/<function_name>", format = "json", data = "<request>")]
 fn dynamic_handler(function_name: String, request: Json<Value>) -> String {
-    let mut modified_request = request.into_inner();
-    modified_request["function_name"] = json!(function_name);
+    let mut request_with_function_name = request.into_inner();
+    request_with_function_name["function_name"] = json!(function_name);
 
     // Serialize the modified request to JSON
-    let serialized_request = match serde_json::to_vec(&modified_request) {
+    let mut serialized_request = match serde_json::to_vec(&request_with_function_name) {
         Ok(data) => data,
         Err(e) => return format!("Error serializing request: {}", e),
     };
 
     // Send this serialized data to the HSM
-    let response_bytes = core_mock::send_to_hsm(serialized_request);
+    if serialized_request.len() > FM_MAX_BUFFER_SIZE {
+        return format!(
+            "HSM buffer size limit is {} but serialized request size is {}",
+            FM_MAX_BUFFER_SIZE,
+            serialized_request.len()
+        );
+    }
+
+    let mut out_buf: [u8; FM_MAX_BUFFER_SIZE] = [0; FM_MAX_BUFFER_SIZE];
+    let mut out_len: u32 = 0;
+
+    let _rv = vault_core::handler(
+        serialized_request.as_mut_ptr(),
+        serialized_request.len() as u32,
+        out_buf.as_mut_ptr(),
+        &mut out_len,
+    );
+
+    if out_len as usize > FM_MAX_BUFFER_SIZE {
+        return format!(
+            "HSM buffer size limit is {} but serialized response size is {}",
+            FM_MAX_BUFFER_SIZE, out_len
+        );
+    }
+
+    let out_buf_slice = unsafe { slice::from_raw_parts(out_buf.as_ptr(), out_len as usize) };
+    let response_bytes = out_buf_slice.to_vec();
 
     // Deserialize the response bytes to JSON
     match String::from_utf8(response_bytes) {
--- vault-core/src/lib.rs
+++ vault-core/src/lib.rs
@@ -8,18 +8,139 @@
 #[cfg(feature = "lunahsm")]
 mod lunahsm_handlers;
 
+use core::{ptr, slice};
+use serde::{Deserialize, Serialize};
+use serde_json::{json, Value};
+
+pub const FM_MAX_BUFFER_SIZE: usize = 1024 * 64;
+
 pub fn handler(in_buf: *mut u8, in_len: u32, out_buf: *mut u8, out_len: &mut u32) -> u32 {
-    let in_buf = unsafe { core::slice::from_raw_parts(in_buf, in_len as usize) };
-    let out_buf = unsafe { core::slice::from_raw_parts_mut(out_buf, *out_len as usize) };
+    let serialized_response = if in_len as usize > FM_MAX_BUFFER_SIZE {
+        get_error_response(&format!(
+            "HSM buffer size limit is {} but serialized request size is {}",
+            FM_MAX_BUFFER_SIZE, in_len
+        ))
+    } else {
+        let in_buf = unsafe { slice::from_raw_parts(in_buf, in_len as usize) };
+        let serialized_request = in_buf.to_vec();
 
-    let in_str = core::str::from_utf8(in_buf).unwrap();
-    let out_str = in_str.to_uppercase();
+        let serialized_response = dispatcher(serialized_request);
 
-    let out_bytes = out_str.as_bytes();
-    let out_len2 = out_bytes.len();
+        if serialized_response.len() > FM_MAX_BUFFER_SIZE {
+            get_error_response(&format!(
+                "HSM buffer size limit is {} but serialized response size is {}",
+                FM_MAX_BUFFER_SIZE,
+                serialized_response.len()
+            ))
+        } else {
+            serialized_response
+        }
+    };
 
-    out_buf[..out_len2].copy_from_slice(out_bytes);
-    *out_len = out_len2 as u32;
+    unsafe {
+        ptr::copy(
+            serialized_response.as_ptr(),
+            out_buf,
+            serialized_response.len(),
+        );
+    }
+    *out_len = serialized_response.len() as u32;
 
     0
+}
+
+fn get_error_response(message: &str) -> Vec<u8> {
+    let error_response = json!({ "error": message });
+    serde_json::to_vec(&error_response).unwrap()
+}
+
+macro_rules! get_or_error {
+    ($request:expr, $field:expr, $type:ty) => {
+        match $request.get($field) {
+            Some(value) => match serde_json::from_value::<$type>(value.clone()) {
+                Ok(valid_value) => valid_value,
+                Err(_) => return get_error_response(&format!("Invalid {}", $field)),
+            },
+            None => return get_error_response(&format!("Missing {}", $field)),
+        }
+    };
+    ($request:expr, $field:expr, Vec<$type:ty>) => {
+        match $request.get($field) {
+            Some(value) => value
+                .as_array()
+                .unwrap_or_else(|| {
+                    return get_error_response(&format!("{} is not an array", $field));
+                })
+                .iter()
+                .map(|v| {
+                    serde_json::from_value::<$type>(v.clone()).unwrap_or_else(|_| {
+                        return get_error_response(&format!("Invalid {}", $field));
+                    })
+                })
+                .collect::<Vec<$type>>(),
+            None => return get_error_response(&format!("Missing {}", $field)),
+        }
+    };
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Reason {
+    name: String,
+    code: u64,
+}
+
+pub fn dispatcher(data: Vec<u8>) -> Vec<u8> {
+    // Deserialize the JSON data into a Value
+    let mut request: Value = match serde_json::from_slice(&data) {
+        Ok(req) => req,
+        Err(e) => {
+            return get_error_response(&format!("Failed to deserialize request: {}", e));
+        }
+    };
+
+    // Extract function_name from the JSON
+    let function_name = match request["function_name"].as_str() {
+        Some(name) => name.to_string(),
+        None => {
+            return get_error_response("Missing function_name");
+        }
+    };
+
+    // Remove function_name from the request
+    request.as_object_mut().unwrap().remove("function_name");
+
+    // Handle the request based on the function_name
+    let response = match function_name.as_str() {
+        "AddVault" => {
+            let label = get_or_error!(request, "label", String);
+            let id = get_or_error!(request, "id", u64);
+            add_vault(label, id)
+        }
+        "RemoveVault" => {
+            let label = get_or_error!(request, "label", String);
+            let reasons = get_or_error!(request, "reasons", Vec<Reason>);
+            let code = get_or_error!(request, "code", u64);
+            remove_vault(label, reasons, code)
+        }
+        _ => Err("Unsupported function".to_string()),
+    };
+
+    // Convert the response to JSON and serialize it
+    match response {
+        Ok(msg) => {
+            let response_json = json!({ "message": msg });
+            serde_json::to_vec(&response_json).unwrap()
+        }
+        Err(e) => get_error_response(&e),
+    }
+}
+
+fn add_vault(label: String, id: u64) -> Result<String, String> {
+    let message = format!("Added vault '{}' with ID {}", label, id);
+    Ok(message)
+}
+
+fn remove_vault(label: String, _reasons: Vec<Reason>, _code: u64) -> Result<String, String> {
+    let message = format!("Removed vault '{}'", label);
+    Ok(message)
 }
